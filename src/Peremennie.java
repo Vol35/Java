f2676e08104c0bad9b47bdccea24a361dddf7fbd
Vol35 <140203124+Vol35@users.noreply.github.com>
2023-10-06 15:16:59 +0300

    Переменные

---
+++ src/Peremennie.java
@@ -0,0 +1,9 @@
+
+public class Peremennie{
+    public static void main(String[] args) {
+        double d = 1.125612;
+        byte b = 127;
+        System.out.println(d);
+        System.out.println(b);
+    }
+}
