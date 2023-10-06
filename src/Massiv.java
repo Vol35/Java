113e2f38982faa160f9ec0066e6234e32cb7ee09
Vol35 <140203124+Vol35@users.noreply.github.com>
2023-10-06 16:22:44 +0300

    Massiv

---
+++ src/Massiv.java
@@ -0,0 +1,13 @@
+public class Massiv {
+    public static void main(String[] args) {
+        int[] m = new int[5];
+        m[0] = 5;
+        m[1] = 2;
+        m[2] = 3;
+        m[3] = 4;
+        m[4] = 1;
+        for (int i = 0; i < 1; i++) {
+            System.out.println(m[0]+m[2]);
+        }
+    }
+}
