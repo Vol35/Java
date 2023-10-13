b947f63b904a65429a5447ffd1c9347361e79b1d
Vol35 <140203124+Vol35@users.noreply.github.com>
2023-10-13 16:40:26 +0300

    Редакция Массива

--- src/Massiv.java
+++ src/Massiv.java
@@ -1,13 +1,8 @@
 public class Massiv {
     public static void main(String[] args) {
-        int[] m = new int[5];
-        m[0] = 5;
-        m[1] = 2;
-        m[2] = 3;
-        m[3] = 4;
-        m[4] = 1;
-        for (int i = 0; i < 1; i++) {
-            System.out.println(m[0]+m[2]);
+        int[] m = {10, 11, 12, 13, 14,};
+        m = new int[]{14, 11, 12, 13, 10,};
+        int k = m[m.length/2];
+            System.out.println(m[0]+k);
         }
     }
-}
